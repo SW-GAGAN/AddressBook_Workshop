2c54d53bbc98d9a194faf94e153797c8198a2af6
SW-GAGAN <srgaganreddy0@gmail.com>
2021-08-21 22:39:52 +0530

    Added: Edit the contacts using first name to Address Book

--- src/test/java/com/bridgelabz/AddressBookTeste.java
+++ src/test/java/com/bridgelabz/AddressBookTeste.java
@@ -1,27 +1,55 @@
 package com.bridgelabz;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddressBookTeste {
     AddressBook addressBook;
+    Contact contact= new Contact("Gagan","Sr","Bengaluru","Karnataka",560099
+            ,966339366,"gagansr@gmail.com");
+    Contact contact1=new Contact("Srini","Rr","Bengaluru","Karnataka",560076
+            ,526157122,"srinivas@gmail.com");
+    Contact newContact =new Contact("Gagan","Rs","indore","MP",
+            21212,6876786,"gaganRs@gmail.com");
+    Contact newContact2 =new Contact("Manuew","re","indore","MP",
+            21212,6876786,"manushetty7799@gmail.com");
 
     @Test
-    public void whenGivenAddressbook_whenAdded_shouldReturnContactList() {
-        addressBook = new AddressBook();
+    public void givenAddressbook_whenAdded_shouldReturnContactList(){
+        addressBook=new AddressBook();
         addressBook.message();
-        Contact contact = new Contact("Gagan", "Reddy", "Bengaluru", "Karnataka", 560099
-                , 872228961, "gaganreddy@gmail.com");
+        Contact contact= new Contact("Gagan","Sr","Bengaluru","Karnataka",560099
+                ,966339366,"gagansr@gmail.com");
+        Contact contact1=new Contact("Srini","Rr","Bengaluru","Karnataka",560076
+                ,526157122,"srinivas@gmail.com");
         addressBook.addNewContact(contact);
-        ArrayList<Contact> contactList = addressBook.getContactlist();
-        Assertions.assertEquals(1, contactList.size());
+        addressBook.addNewContact(contact1);
+        ArrayList<Contact> contactList= addressBook.getContactlist();
+        Assertions.assertEquals(2,contactList.size());
     }
 
+
     @Test
-    public void givenContactsDataFromConsole_WhenAdded_shouldReturnTrue() {
-        addressBook = new AddressBook();
+    public void givenContactsDataFromConsole_WhenAdded_shouldReturnTrue(){
+        addressBook=new AddressBook();
         Assertions.assertTrue(addressBook.addNewContactFromConsole());
     }
+
+    @Test
+    public  void givenContactsData_whenAdded_shouldReturnTrue(){
+        addressBook=new AddressBook();
+        addressBook.addNewContact(contact);
+        addressBook.addNewContact(contact1);
+        Contact oldContact=addressBook.getContact("Gagan","Sr");
+        Contact oldContact2=addressBook.getContact("gurasa","Er");
+        Assertions.assertEquals(null,oldContact2);
+        Assertions.assertTrue(addressBook.updateContact(oldContact,newContact));
+        Assertions.assertEquals(newContact,addressBook.getContact(newContact));
+        Assertions.assertEquals(false,addressBook.updateContact(oldContact2,newContact2));
+    }
+
 }
