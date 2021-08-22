1cdb452ab4047e493d15599195d8f04df62632a8
SW-GAGAN <srgaganreddy0@gmail.com>
2021-08-22 21:22:13 +0530

    Added: Add multiple Contact to Address Book

--- src/test/java/com/bridgelabz/AddressBookTeste.java
+++ src/test/java/com/bridgelabz/AddressBookTeste.java
@@ -9,27 +9,27 @@ import java.util.Scanner;
 
 public class AddressBookTeste {
     AddressBook addressBook;
-    Contact contact = new Contact("Gagan", "Sr", "Bengaluru", "Karnataka", 560099
-            , 966339366, "gagansr@gmail.com");
-    Contact contact1 = new Contact("Srini", "Rr", "Bengaluru", "Karnataka", 560076
-            , 526157122, "srinivas@gmail.com");
-    Contact newContact = new Contact("Gagan", "Rs", "indore", "MP",
-            21212, 6876786, "gaganRs@gmail.com");
-    Contact newContact2 = new Contact("Manuew", "re", "indore", "MP",
-            21212, 6876786, "manushetty7799@gmail.com");
+    Contact contact= new Contact("Gagan","Sr","Bengaluru","Karnataka",560076
+            ,966339366,"gagansr@gmail.com");
+    Contact contact1=new Contact("Srinivas","Kv","Bengaluru","Karnataka",560076
+            ,526157122,"srinivas@gmail.com");
+    Contact newContact =new Contact("Gagan","re","indore","MP",
+            21212,6876786,"manushetty7799@gmail.com");
+    Contact newContact2 =new Contact("Manuew","re","indore","MP",
+            21212,6876786,"manushetty7799@gmail.com");
 
     @Test
-    public void givenAddressbook_whenAdded_shouldReturnContactList() {
-        addressBook = new AddressBook();
+    public void givenAddressbook_whenAdded_shouldReturnContactList(){
+        addressBook=new AddressBook();
         addressBook.message();
-        Contact contact = new Contact("Gagan", "Sr", "Bengaluru", "Karnataka", 560099
-                , 966339366, "gagansr@gmail.com");
-        Contact contact1 = new Contact("Srini", "Rr", "Bengaluru", "Karnataka", 560076
-                , 526157122, "srinivas@gmail.com");
+        Contact contact= new Contact("Gagan","Sr","Bengaluru","Karnataka",560076
+                ,966339366,"manukvshetty@gmail.com");
+        Contact contact1=new Contact("Srinivas","Kv","Bengaluru","Karnataka",560076
+                ,526157122,"srinivas@gmail.com");
         addressBook.addNewContact(contact);
         addressBook.addNewContact(contact1);
-        ArrayList<Contact> contactList = addressBook.getContactlist();
-        Assertions.assertEquals(2, contactList.size());
+        ArrayList<Contact> contactList= addressBook.getContactlist();
+        Assertions.assertEquals(2,contactList.size());
     }
 
 
@@ -44,42 +44,31 @@ public class AddressBookTeste {
         addressBook=new AddressBook();
         addressBook.addNewContact(contact);
         addressBook.addNewContact(contact1);
-        Contact oldContact = addressBook.getContact("Gagan", "Sr");
-        Contact oldContact2 = addressBook.getContact("gurasa", "Er");
-        Assertions.assertEquals(null, oldContact2);
-        Assertions.assertTrue(addressBook.updateContact(oldContact, newContact));
-        Assertions.assertEquals(newContact, addressBook.getContact(newContact));
-        Assertions.assertEquals(false, addressBook.updateContact(oldContact2, newContact2));
+        Contact oldContact=addressBook.getContact("Gagan","Sr");
+        Contact oldContact2=addressBook.getContact("gurasa","Er");
+        Assertions.assertEquals(null,oldContact2);
+        Assertions.assertTrue(addressBook.updateContact(oldContact,newContact));
+        Assertions.assertEquals(newContact,addressBook.getContact(newContact));
+        Assertions.assertEquals(false,addressBook.updateContact(oldContact2,newContact2));
     }
 
     @Test
-    public void givenContactsDataFromConsole_WhenAdded_shouldReturnTrue() {
-        addressBook = new AddressBook();
-        Assertions.assertTrue(addressBook.addNewContactFromConsole());
-    }
-
-    @Test
-    public void givenContactsData_whenAdded_shouldReturnTrue() {
-        addressBook = new AddressBook();
+    public  void givenContactsData_whenDeletedContact_shouldReturnDeletedContact(){
+        addressBook =new AddressBook();
         addressBook.addNewContact(contact);
         addressBook.addNewContact(contact1);
-        Contact oldContact = addressBook.getContact("Manu", "Kv");
-        Contact oldContact2 = addressBook.getContact("gurasa", "Er");
-        Assertions.assertEquals(null, oldContact2);
-        Assertions.assertTrue(addressBook.updateContact(oldContact, newContact));
-        Assertions.assertEquals(newContact, addressBook.getContact(newContact));
-        Assertions.assertEquals(false, addressBook.updateContact(oldContact2, newContact2));
-    }
-
-    @Test
-    public void givenContactsData_whenDeletedContact_shouldReturnDeletedContact() {
-        addressBook = new AddressBook();
-        addressBook.addNewContact(contact);
-        addressBook.addNewContact(contact1);
-        boolean positiveResult = addressBook.deleteContact("Gagan", "Sr");
+        boolean positiveResult=addressBook.deleteContact("Gagan","Sr");
         Assertions.assertTrue(positiveResult);
-        boolean negativeResult2 = addressBook.deleteContact("Manu", "Kv");
-        Assertions.assertEquals(false, negativeResult2);
+        boolean negativeResult2=addressBook.deleteContact("Gagan","Sr");
+        Assertions.assertEquals(false,negativeResult2);
+    }
+
+    @Test
+    public  void givenMultipleContactsData_whenAddedContact_shouldReturnSizeOfAddressBook(){
+        addressBook =new AddressBook();
+        Contact[] contactsArray={contact,contact1,newContact,newContact2,contact};
+        int numberOfContactsAdded=addressBook.addMultipleContacts(contactsArray);
+        Assertions.assertEquals(4,numberOfContactsAdded);
     }
 
 }
