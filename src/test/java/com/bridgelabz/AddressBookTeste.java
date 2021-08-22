791ce45889fa6679aa254095a6c82df13a89f653
SW-GAGAN <srgaganreddy0@gmail.com>
2021-08-22 21:51:57 +0530

    Added: Search person using City among multiple Contact to Address Book

--- src/test/java/com/bridgelabz/AddressBookTeste.java
+++ src/test/java/com/bridgelabz/AddressBookTeste.java
@@ -104,4 +104,14 @@ public class AddressBookTeste {
         List<Contact> contactListInCity2=multipleAdressBookContainer.getContactsByCity("mysore");
         Assertions.assertEquals(0,contactListInCity2.size());
     }
+    @Test
+    public void givenMultipleAddressBooks_whenSearchedForParticularState_shouldReturnContacts2(){
+        Contact[] contactsArray={contact,contact1,newContact,contact};
+        addressBookFriends.addMultipleContacts(contactsArray);
+        List<Contact> contactListInState=addressBookFriends.getContactsByState("MP");
+        Assertions.assertEquals("MP",contactListInState.get(0).state);
+        List<Contact> contactListInState2=addressBookFriends.getContactsByState("kerala");
+        Assertions.assertEquals(0,contactListInState2.size());
+
+    }
 }
